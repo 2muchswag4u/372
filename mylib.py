c4f1853eeb9adf7d38fe1e1615e4fa4b68eea112
2muchswag4u <42687255+2muchswag4u@users.noreply.github.com>
2018-09-04 23:31:58 -0700

    Add files via upload

---
+++ mylib.py
@@ -0,0 +1,14 @@
+def sumar(x,y):
+    return x+y
+
+x = int(input("Escribe un numero"))
+y =int(input("Escribe otro numero"))
+def add(num2=1, num=1):
+    num= num + num2
+
+    return num
+def multi(x,y):
+    return x*y
+
+def restar(x,y):
+    return x-y
